48535dbcd83cbe4871957952e405755383a34e88
Kristen-Beer <Krismason56@gmail.com>
2023-12-10 12:42:07 -0800

    get geojason url

---
+++ Starter_Code/static/js/logic.js
@@ -0,0 +1,52 @@
+// map background tile layer
+console.log("Step 1 working");
+let basemap = L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
+    {
+      attribution:
+        'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    });
+
+// Map object
+let map = L.map("map", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 3
+  });
+
+  basemap.addTo(map);
+
+  //Retreive geoJSON data
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson")
+
+  // Style data for magnitude
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  //marker colors
+  function getColor(depth) {
+    switch (true) {
+      case depth > 90:
+        return "#ea2c2c";
+      case depth > 70:
+        return "#ea822c";
+      case depth > 50:
+        return "#ee9c00";
+      case depth > 30:
+        return "#eecc00";
+      case depth > 10:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
+    }
+  }
