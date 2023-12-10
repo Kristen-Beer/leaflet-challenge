8fa4e3122952c32d8b1659b5f64f551bb96c9b7c
Kristen-Beer <Krismason56@gmail.com>
2023-12-10 13:04:17 -0800

    added legend to map

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -15,38 +15,98 @@ let map = L.map("map", {
     zoom: 3
   });
 
-  basemap.addTo(map);
+//Add basemap to map
+basemap.addTo(map);
 
-  //Retreive geoJSON data
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson")
+//Retreive geoJSON data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function (data) {
 
-  // Style data for magnitude
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.geometry.coordinates[2]),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-  }
+    // Style data for magnitude
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
 
-  //marker colors
-  function getColor(depth) {
-    switch (true) {
-      case depth > 90:
-        return "#ea2c2c";
-      case depth > 70:
-        return "#ea822c";
-      case depth > 50:
-        return "#ee9c00";
-      case depth > 30:
-        return "#eecc00";
-      case depth > 10:
-        return "#d4ee00";
-      default:
-        return "#98ee00";
+    //marker colors
+    function getColor(depth) {
+        switch (true) {
+            case depth > 90:
+                return "#ea2c2c";
+            case depth > 70:
+                return "#ea822c";
+            case depth > 50:
+                return "#ee9c00";
+            case depth > 30:
+                return "#eecc00";
+            case depth > 10:
+                return "#d4ee00";
+            default:
+                return "#98ee00";
     }
   }
+
+
+  //Radius of Markers based on magnitude
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 4;
+  }
+
+  //Add GEOJSON layer to the map
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMaker(latlng);
+    },
+    // Style and popups
+    style: styleInfo,
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(
+          "Magnitude: "
+          + feature.properties.mag
+          + "<br>Depth: "
+          + feature.geometry.coordinates[2]
+          + "<br>Location: "
+          + feature.properties.place
+        );
+      }
+    }).addTo(map);
+  
+    //Legend
+    let Legend = L.control({
+        position: "bottomright"
+    });
+
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+    
+        let grades = [-10, 10, 30, 50, 70, 90];
+        let colors = [
+          "#98ee00",
+          "#d4ee00",
+          "#eecc00",
+          "#ee9c00",
+          "#ea822c",
+          "#ea2c2c"
+        ];
+        
+    // Generate a colored square for each interval
+    for (let i = 0; i < grades.length; i++) {
+        div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+          + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      }
+      return div;
+    };
+
+    //Add legend to map
+    legend.addTo(map);
+});
